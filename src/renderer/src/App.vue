8fb34f1d9437faa09fb952b493fae0b1dcae5844
rerades <rerades@siete3.com>
2024-03-22 23:06:57 +0100

    adding router

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,26 +1,19 @@
 <script setup lang="ts">
 import Versions from './components/Versions.vue'
-
-const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+import { RouterLink, RouterView, useRoute } from 'vue-router'
+const route = useRoute()
 </script>
 
 <template>
-  <img alt="logo" class="logo" src="./assets/electron.svg" />
-  <div class="creator">Powered by electron-vite</div>
-  <div class="text">
-    Build an Electron app with
-    <span class="vue">Vue</span>
-    and
-    <span class="ts">TypeScript</span>
-  </div>
-  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
-  <div class="actions">
-    <div class="action">
-      <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
-    </div>
-    <div class="action">
-      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
-    </div>
-  </div>
+  <strong>Current route path:</strong> {{ route.fullPath }}
+  <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink>
+  </nav>
+  <hr />
+  <main>
+    <RouterView />
+  </main>
+  <hr />
   <Versions />
 </template>
--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -2,6 +2,8 @@ import './assets/main.css'
 import '../../../node_modules/flowbite-vue/dist/index.css'
 
 import { createApp } from 'vue'
+
+import router from './router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
